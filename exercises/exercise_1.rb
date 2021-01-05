715c656c0449a30def6273daca616b467e432fc1
vagrant <avedj94@hotmail.com>
2021-01-05 02:59:00 +0000

    create 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,15 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+b = Store.new
+b.name = "Burnaby"
+b.annual_revenue = 300000
+b.mens_apparel = true
+b.womens_apparel = true
+b.save
+
+r = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+g = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
