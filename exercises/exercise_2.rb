7e68941c0e366872f890c2ff3132515a113f0fac
vagrant <avedj94@hotmail.com>
2021-01-05 03:08:51 +0000

    update the first store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+store1 = Store.find_by(id: '1')
+@store1 = store1
+store1.update(name: 'Eglington')
+
+
+store2 = Store.find_by(id: '2')
+store2 = @store2
+
+puts store1.name
